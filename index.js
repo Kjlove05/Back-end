f1598bf5aa1e7bdd1eb92e0e8b4687524e3d451c
Kyle Joshua <kj1600053@gmail.com>
2022-06-30 16:27:00 +0800

    debug idk

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
+
 
 
 let notes = [
@@ -84,7 +88,7 @@ let notes = [
   
     response.json(note)
   })
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
